b9f9574171651f7bd10a40178cf33b9141643cb6
Whiskers75 - <crazycats@yopmail.com>
2012-07-28 08:19:50 -0400

    Fixing various bugs

--- server.js
+++ server.js
@@ -22,7 +22,7 @@
         });
         
         exports.send = function send(message, socket) {
-            sockets[sockets.indexOf(socket)].write(message, '\n');
+            sockets[socket].write(message, '\n');
         };
         
         socket.on('end', function() {

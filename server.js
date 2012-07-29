2225faaf26a8e3511529176571904c7cdaf2b926
Whiskers75 - <crazycats@yopmail.com>
2012-07-29 04:35:56 -0400

    Fixing bugs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
     var sockets = [];
     var listenerport;
     var msgTerminator = '\n';
-    var buf;
+    var buf = '';
 
     
     listenerport = 8080; // Port to listen on
@@ -17,25 +17,16 @@
         sockets.push(socket);
         
         socket.on('data', function(data){
-// add new data to your buffer
         buf += data;
-
-    // see if there is one or more complete messages
         if (buf.instr(msgTerminator) >= 0) {
-        // slice up the buffer into messages
             var msgs = data.split(msgTerminator);
-
             for (var i = 0; i < msgs.length - 2; ++i) {
-            // walk through each message in order
             var msg = msgs[i];
-
-            // pick off the current message
             console.log('Data in server, sending to handle()');
-            // send only the current message to your handler
             worker.handle(msg, socket);
         }
 
-        buf = msgs[msgs.length - 1];  // put back any partial message into your buffer
+        buf = msgs[msgs.length - 1];
 
         }
         

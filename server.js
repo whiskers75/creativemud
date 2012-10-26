a9cb5fd7d131f7a09e47109f4fc57fc02673b6b9
whiskers75 <crazycats@yopmail.com>
2012-10-26 04:51:46 -0400

    Added shiny new login stuff - test 1

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ var net = require('net');
 var len = 0;
 var args;
 var password = require('../config.js');
-var version = "0.0.3";
-
+var version = "Alpha0.2";
+var log = function(data) {
+    console.log(data);
+    // Will add file logging later
+};
 db.auth(password, function() { // For auth, comment out if not needed..
     console.log('Auth\'d');
     db.on('error', function(err) {
@@ -68,22 +71,37 @@ var setAreaMetadata = function(area, meta, val) {
 var startsWith = function (superstr, str) {
   return !superstr.indexOf(str);
 };
+var doesNameExist = function(name) {
+    db.get(name + ':name', function(err,res) {
+        if (err) {
+            return 'Error';
+        }
+        else {
+            if (res === null) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    });
+};
 
-var login = function(name, socket, passcode, callback) { 
+var login = function(name, socket, passcode) { 
     db.get(name + ':name', function(err, res) {
         if (err) {
-            return callback('Error: ' + err);
+            return 'Error: ' + err;
         }
         else {
             var res2 = res;
             db.get(name + ':pin', function(err, res) {
                 if (err) {
-                    return callback('Error: ' + err);
+                    return 'Error: ' + err;
                 }
                 else if (name === res2) {
                     if (passcode === res) {
                         players[sockets.indexOf(socket)] = name;
-                        return callback('Logged in as: ' + players[sockets.indexOf(socket)]);
+                        return 'Logged in as: ' + players[sockets.indexOf(socket)];
                     }
                     else {
                         
@@ -96,35 +114,41 @@ var login = function(name, socket, passcode, callback) {
         }
     });
 };
-var register = function(name, socket, passcode, callback) {
+
+var mkPrompt = function(user) {
+    // Prompt maker, will edit later
+    return 'CreativeMUD Alpha0.2>';
+};
+
+var register = function(name, socket, passcode) {
     if (name === null) {
-        return callback('Name null!');
+        return 'Name null!';
     }
     else if (name === '') {
-        return callback('Name empty!');
+        return 'Name empty!';
     }
     else {
         db.get(name + ':name', function(error, res) {
             if (error) {
-                return callback('Error: ' + error);
+                return'Error: ' + error;
             }
             else if (res !== null) {
-                return callback('This name has been taken!');
+                return 'This name has been taken!';
             }
             else {
                 db.set(name + ':name', name, function(error) {
                     if (error) {
-                        return callback('Error: ' + error);
+                        return 'Error: ' + error;
                     }
                     else {
                         db.set(name + ':pin', passcode, function(error) {
                             if (error) {
-                                return callback('Error: ' + error);
+                                return 'Error: ' + error;
                             }
                             else {
                                 players[sockets.indexOf(socket)] = name;
                                 init(players[sockets.indexOf(socket)]);
-                                callback('Logged in as: ' + players[sockets.indexOf(socket)]);
+                                return 'Logged in as: ' + players[sockets.indexOf(socket)];
                             }
                         });
                     }
@@ -135,12 +159,58 @@ var register = function(name, socket, passcode, callback) {
 };
 
 net.createServer(function (socket) {
-    socket.write('Welcome to CreativeMUD, version '+version+'\nThere are currently '+ len + ' players logged in.\nTo exit CreativeMUD, type \'.exit\'.\nIf CreativeMUD seems to freeze, type \'.break\'.\nType \'help\' for help.\n');
+    socket.write('Welcome to CreativeMUD, version '+version+'.\nThere are currently '+ len + ' players logged in.\nTo exit CreativeMUD, type \'.exit\'.\nIf CreativeMUD seems to freeze, type \'.break\'.\nType \'help\' for help.\n');
+    socket.write('What is your name?');
+    socket.setEncoding('utf-8');
+    socket.setTimeout(300000);
+    socket.on('timeout', function() {
+        socket.write('Timed out after 5 minutes.');
+        socket.end();
+        len = len - 1;
+    });
     sockets.push(socket);
-    players[sockets.indexOf(socket)] = 'none';
+    socket.once('data', function(data) {
+        if (data === null) {
+            socket.write('Goodbye.');
+            socket.end();
+        }
+        else {
+            if (!doesNameExist(data)) {
+                socket.write('It looks like that might be a new name. Would you like to register? (y/n)');
+                socket.once('data', function(data2) {
+                    if (data2 === 'y') {
+                        socket.write('Well then. Please enter a passcode.');
+                        socket.once('data', function(data3) {
+                            socket.write(register(data, socket, data3));
+                            socket.write('Welcome to CreativeMUD.');
+                        });
+                    }
+                    else {
+                        socket.write('Goodbye.');
+                        socket.end();
+                    }
+                });
+            }
+            else {
+                if (doesNameExist(data)) {
+                    if (doesNameExist(data) !== 'Error') {
+                        socket.write('Welcome back, '+data+'. What is your passcode?');
+                            socket.once('data', function(data4) {
+                                login(data, socket, data4);
+                            });
+                    }
+            }
+        }
+    }
+    
+                
+        
+    // DEPRECATED:
+    // players[sockets.indexOf(socket)] = 'none';
     len = len + 1;
+    socket.write(mkPrompt(players[sockets.indexOf(socket)]));
     repl.start({
-        prompt:"CreativeMUD, socket "+ sockets.indexOf(socket)+"> ",
+        prompt: "", // Uses mkprompt() now
         'input': socket,
         'output': socket,
         'writer': function(object) {
@@ -150,21 +220,21 @@ net.createServer(function (socket) {
             cmd = cmd.replace("\n)","").replace("(","");
             // console.log(cmd);
             args = cmd.split(" ");
-            
-            if (startsWith(cmd, 'login')) {
-                login(args[1], socket, args[2], function(data) {
-                    callback(null, data);
-                });
-            }
-            if (startsWith(cmd, 'logout')) {
-                players[sockets.indexOf(socket)] = 'none';
-                callback(null, 'Logged out');
-            }
-            if (startsWith(cmd, 'register')) {
-                register(args[1], socket, args[2], function(data) {
-                    callback(null, data);
-                });
-            }
+            // DEPRECATED:
+            //if (startsWith(cmd, 'login')) {
+            //    login(args[1], socket, args[2], function(data) {
+            //        callback(null, data);
+            //    });
+            //}
+            //if (startsWith(cmd, 'logout')) {
+            //    players[sockets.indexOf(socket)] = 'none';
+            //    callback(null, 'Logged out');
+            //}
+            //if (startsWith(cmd, 'register')) {
+            //    register(args[1], socket, args[2], function(data) {
+            //        callback(null, data);
+            //    });
+            //}
             if (cmd === "look") {
                 callback(null, getAreaMetadata(getArea(players[sockets.indexOf(socket)]), 'title') + '\n' + getAreaMetadata(getArea(players[sockets.indexOf(socket)]), 'desc'));
             }
@@ -175,9 +245,10 @@ net.createServer(function (socket) {
                 socket.end();
             }
             if (cmd === "help") {
-                callback(null, 'Help\nLogin with login [username] [pin]\nRegister with register [username] [pin]\nQuit with quit');
+                callback(null, 'Help\nLook with look\nQuit with quit');
             }
             // put new commands here...
+            socket.write(mkPrompt(players[sockets.indexOf(socket)]));
     }}).on('exit', function() {
         socket.end();
         len = len - 1;

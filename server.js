76f5f3939f6c20860b9241f9085b718a89d7c9d8
whiskers75 <crazycats@yopmail.com>
2012-10-27 12:23:14 -0400

    Finally fixed the login (hopefully)- test 8

--- server.js
+++ server.js
@@ -166,25 +166,8 @@ net.createServer(function (socket) {
     sockets.push(socket);
     var streams = [];
     var bytes = 0;
-    streams[sockets.indexOf(socket)] = new Stream();
-    streams[sockets.indexOf(socket)].readable = true;
-    streams[sockets.indexOf(socket)].writable = true;
-    streams[sockets.indexOf(socket)].pause = function() {}
-    streams[sockets.indexOf(socket)].resume = function() {}
-    streams[sockets.indexOf(socket)].write = function (buf) {
-    bytes += buf.length;
-    };
+    streams[sockets.indexOf(socket)] = require('through');
 
-    streams[sockets.indexOf(socket)].end = function (buf) {
-    if (arguments.length)  streams[sockets.indexOf(socket)].write(buf);
-
-    streams[sockets.indexOf(socket)].writable = false;
-    console.log(bytes + ' bytes written');
-    };
-
-    streams[sockets.indexOf(socket)].destroy = function () {
-         streams[sockets.indexOf(socket)].writable = false;
-    };
     socket.on('connect', function(socket) {
         log('Socket '+sockets.indexOf(socket)+' connected.');
     });
@@ -205,8 +188,7 @@ net.createServer(function (socket) {
     });
     readlines[sockets.indexOf(socket)] = rl.createInterface({
         input: streams[sockets.indexOf(socket)],
-        output: socket,
-        terminal: true
+        output: socket
     });
     readlines[sockets.indexOf(socket)].on('SIGINT', function() {
         socket.end();

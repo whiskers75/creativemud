0a9ba48b685450d74c5e4d8edb6394bc1c977344
whiskers75 <crazycats@yopmail.com>
2012-10-28 07:17:36 -0400

    Added looking on login

--- server.js
+++ server.js
@@ -261,6 +261,18 @@ net.createServer(function (socket) {
     // players[sockets.indexOf(socket)] = 'none';
     len = len + 1;
     var startREPL = function() {
+        getAttr(players[sockets.indexOf(socket)], 'area', function(area) {
+            getAttr('area_' + area, 'title', function(title) {
+                getAttr('area_' + area, 'desc', function(desc) {
+                    getAttr('area_' + area, 'exits', function(exits) {
+                        socket.write(title + '\n' + desc + '\n' + exits + '\n');
+                            mkPrompt(players[sockets.indexOf(socket)], function(result) {
+                                socket.write(result);
+                            });
+                        }); 
+                    });
+              });
+        });
         setTimeout(function() {
             mkPrompt(players[sockets.indexOf(socket)], function(result) {
                 socket.write(result);
